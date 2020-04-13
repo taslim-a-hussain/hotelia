a8a563920b8cdc4b468291ff9419b1ebf3d0b884
Taslim A Hussain <taslim@outlook.co.nz>
2020-04-13 20:48:15 +1200

    Updates

---
+++ precache-manifest.8c3703f8d2c186463c42dc6b2ccfe996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8af51e19e3e14fb621ca8f69b213f4",
+    "url": "/grand-hotel/index.html"
+  },
+  {
+    "revision": "7776383e744ee89d749f",
+    "url": "/grand-hotel/static/css/main.6bf1c80d.chunk.css"
+  },
+  {
+    "revision": "8df9f787d6ff3e50b7bf",
+    "url": "/grand-hotel/static/js/2.97ebd481.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/grand-hotel/static/js/2.97ebd481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7776383e744ee89d749f",
+    "url": "/grand-hotel/static/js/main.5c07f037.chunk.js"
+  },
+  {
+    "revision": "f52548963431f59794e6",
+    "url": "/grand-hotel/static/js/runtime-main.a11b8d4c.js"
+  }
+]);
--- src/components/Home/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default () => (
-    <div>
-        <h1>Home</h1>
-        <p>Here is some dummy text....</p>
-    </div>
-);
