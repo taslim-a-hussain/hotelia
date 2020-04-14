13d4e3843c218e04fbd221291045819e0745ea9b
Taslim A Hussain <taslim@outlook.co.nz>
2020-04-14 22:51:31 +1200

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,12 +7,15 @@ export default () => {
     const date = new Date();
     return (
     <div className="navbar">
+        <input type="checkbox" className="checkbox" id="click" />
         <div className="sidebar">
+            <label for="click">
             <div className="menu-icon">
                 <div className="line line-1"></div>
                 <div className="line line-2"></div>
                 <div className="line line-3"></div>
             </div>
+            </label>
             <Social />
             <div className="year">
                 {date.getFullYear()}
