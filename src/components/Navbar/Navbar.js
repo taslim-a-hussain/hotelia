99e9bdd8d59c85085924abad12c4893c5ef7b4bc
Taslim A Hussain <taslim@outlook.co.nz>
2020-04-15 12:41:50 +1200

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Social from '../Social/Social';
+import Social from './Social/Social';
 import Navigation from './Navigation/Navigation';
 import './Navbar.scss';
 
