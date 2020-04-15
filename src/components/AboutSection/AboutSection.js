2cc4a94d77931253b32e7216321d058f05445080
Taslim A Hussain <taslim@outlook.co.nz>
2020-04-15 16:23:44 +1200

    about section

---
+++ src/components/AboutSection/AboutSection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './AboutSection.scss';
+
+
+export default () => (
+    <div className="about-section">
+        <section>
+            <h2>ABOUT US</h2>
+            <div className="lines">
+                <div className="line-1"></div>
+                <div className="line-2"></div>
+            </div>
+            <h2 className="brand">Hotelia</h2>
+            <p>
+                Exercitation esse non sit ea quis duis eiusmod eiusmod do occaecat tempor aliqua aliqua in. Excepteur mollit nisi sunt eiusmod et deserunt consequat exercitation ullamco ut esse ex est qui. Cillum consectetur voluptate dolore ut. Amet laborum aute aliqua dolor aliquip consequat cupidatat consectetur do deserunt do non sint incididunt.
+            </p>
+
+            <button>READ MORE <i className="fas fa-angle-double-right"></i></button>
+        </section>
+    </div>
+);
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Header from '../Header/Header';
+import AboutSection from '../AboutSection/AboutSection';
 
 export default () => (
     <div>
         <Navbar />
         <Header />
+        <AboutSection />
     </div>
 );
